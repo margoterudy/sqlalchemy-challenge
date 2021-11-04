bd0852de7ba6f2ac9cbcb4d860c31aacfee0cdae
Margot <margoterudy@gmail.com>
2021-11-04 15:25:26 -0500

    margot app.py start

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-test
+#step1
+from flask import Flask, jsonify
+
+
+#############################
+# Flask Setup
+#############################
+#step2
+app = Flask(__name__)
+
+
+
+
+
+# step 3 Run code from command line
+if __name__ == '__main__':
+    app.run(debug=True)
